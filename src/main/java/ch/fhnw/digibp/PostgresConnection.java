09b7e58e9bafc7f29fcc31139c0931fd1a581612
Oliver <oliver.ruggli@students.fhnw.ch>
2019-05-22 17:54:58 +0200

    added working postgres connection

--- src/main/java/ch/fhnw/digibp/PostgresConnection.java
+++ src/main/java/ch/fhnw/digibp/PostgresConnection.java
@@ -1,6 +1,5 @@
 package ch.fhnw.digibp;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,30 +7,39 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class PostgresConnection {
+
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "postgres")) {
 
+
+
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/dd786fen583a5s?ssl=true\n", "mtqjbarwkwzsld", "2b51f440930a3711f9cabf9884d82feda3b70e9b35c9e9fb2d5f6f989fef445a"))
+        {
             System.out.println("Java JDBC PostgreSQL Example");
-            // When this class first attempts to establish a connection, it automatically loads any JDBC 4.0 drivers found within
-            // the class path. Note that your application must manually load any JDBC drivers prior to version 4.0.
-//          Class.forName("org.postgresql.Driver");
-
             System.out.println("Connected to PostgreSQL database!");
+
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select c_id   from customer ORDER BY c_id desc");
             while (resultSet.next()) {
-                System.out.printf(resultSet);
+                System.out.println(resultSet.getInt("c_id"));
             }
 
-        } /*catch (ClassNotFoundException e) {
-            System.out.println("PostgreSQL JDBC driver not found.");
-            e.printStackTrace();
-        }*/ catch (SQLException e) {
+        }catch (SQLException e) {
             System.out.println("Connection failure.");
             e.printStackTrace();
-
-
         }
+
+        StringSplitter("1012.4;1013.1;1012.2;1012.1;");
     }
 
+
+    private static void StringSplitter(String OrderChaos){
+
+        String[] getAllOrders = new String[15];
+
+
+
+        String firstPart = OrderChaos.split(";")[0];
+        System.out.println(firstPart.toString());
+
+    }
 }
