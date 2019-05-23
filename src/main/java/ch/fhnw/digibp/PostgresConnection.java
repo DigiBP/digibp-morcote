ee8739e8686c23d060e70bb8996c6fbabc3535e3
LorisTheBoss <loris.grether@students.fhnw.ch>
2019-05-23 14:01:17 +0200

    added String spliter

--- src/main/java/ch/fhnw/digibp/PostgresConnection.java
+++ src/main/java/ch/fhnw/digibp/PostgresConnection.java
@@ -5,19 +5,21 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PostgresConnection {
 
     public static void main(String[] args) {
 
-
+        dbConnect();
 
 
     }
 
-    public static void dbConnect(){
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/dd786fen583a5s?ssl=true\n", "mtqjbarwkwzsld", "2b51f440930a3711f9cabf9884d82feda3b70e9b35c9e9fb2d5f6f989fef445a"))
-        {
+    public static void dbConnect() {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/dd786fen583a5s?ssl=true\n", "mtqjbarwkwzsld", "2b51f440930a3711f9cabf9884d82feda3b70e9b35c9e9fb2d5f6f989fef445a")) {
             System.out.println("Java JDBC PostgreSQL Example");
             System.out.println("Connected to PostgreSQL database!");
 
@@ -27,19 +29,28 @@ public class PostgresConnection {
                 System.out.println(resultSet.getInt("c_id"));
             }
 
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Connection failure.");
             e.printStackTrace();
         }
 
-        StringSplitter("1012.4;1013.1;1012.2;1012.1;");
+        StringSplitter("1012.4;1013.1;1017.2;1019.1;");
     }
 
 
-    private static void StringSplitter(String OrderChaos){
-        String[] getAllOrders = new String[15];
-        String firstPart = OrderChaos.split(";")[0];
-        System.out.println(firstPart.toString());
+    private static void StringSplitter(String orderChaos) {
 
+        Map<String, String> ordersList = new HashMap<>();
+
+        String[] parts = orderChaos.split(";");
+
+        for (int i = 0; i < parts.length; i++) {
+
+            String[] order = parts[i].split("\\.");
+
+            ordersList.put(order[0], order[1]);
+        }
+
+        System.out.println(ordersList);
     }
 }
