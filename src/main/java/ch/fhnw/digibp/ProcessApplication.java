10024e0e7d37a8e08395e75a33deb2d094df9de1
LorisTheBoss <loris.grether@students.fhnw.ch>
2019-05-23 10:11:44 +0200

    Update ProcessApplication.java
    crosssite scripting fix

--- src/main/java/ch/fhnw/digibp/ProcessApplication.java
+++ src/main/java/ch/fhnw/digibp/ProcessApplication.java
@@ -1,15 +1,57 @@
 package ch.fhnw.digibp;
 
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import org.springframework.web.filter.CorsFilter;
+
 @SpringBootApplication
+
 @EnableProcessApplication
+
 public class ProcessApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProcessApplication.class, args);
-	}
+public static void main(String[] args) {
+
+SpringApplication.run(ProcessApplication.class, args);
+
+}
+
+@Bean
+
+public FilterRegistrationBean processCorsFilter() {
+
+UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+CorsConfiguration config = new CorsConfiguration();
+
+config.setAllowCredentials(true);
+
+config.addAllowedOrigin("*");
+
+config.addAllowedHeader("*");
+
+config.addAllowedMethod("*");
+
+source.registerCorsConfiguration("/**", config);
+
+FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+bean.setOrder(0);
+
+return bean;
+
+}
 
 }
