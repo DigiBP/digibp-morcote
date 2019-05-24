dc37291f2956e6c843f3016def6891e955d1c552
Oliver <oliver.ruggli@students.fhnw.ch>
2019-05-24 14:34:46 +0200

    implemented String splitter in PostgressMasterClass --> INSERT INTO user --> INSERT INTO shoppingcard --> Split String and INSERT INTO orders

--- src/main/java/ch/fhnw/digibp/PostgressMasterClass.java
+++ src/main/java/ch/fhnw/digibp/PostgressMasterClass.java
@@ -1,15 +1,21 @@
 package ch.fhnw.digibp;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PostgressMasterClass {
 
     private static int userID;
+    private static int scID;
+    private static String orderChaos="1012.4;1013.1;1017.2;1019.1;";
+
 
     public static void main(String[] args) {
 
-    addUser("Loris", "Yannick","loris@yanniick.com", "FHNW-weg","25", 4600, "olten" );
-    addShoppingCart();
+        addUser("Loris", "Yannick","loris@yanniick.com", "FHNW-weg","25", 4600, "olten" );
+        addShoppingCart();
+        orderCreation(orderChaos);
 
     }
 
@@ -57,24 +63,21 @@ public class PostgressMasterClass {
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("select sc_id   from shoppingcart ORDER BY sc_id desc");
 
-            int newSCID;
 
             if (rs.next()){
-                newSCID =rs.getInt("sc_id");
-                newSCID+=1;
-
+                setscID(rs.getInt("sc_id")+1);
             }
             else{
-                newSCID=0;
+                setscID(0);
             }
 
 
             if (getUserID() > 0) {
-                String query = "INSERT INTO shoppingcart (\"sc_id\", \"fk_customer_id\", \"sc_cumulatedprice_swissrappen\") VALUES ('"+newSCID+"', '"+getUserID()+"', '"+0+"')";
+                String query = "INSERT INTO shoppingcart (\"sc_id\", \"fk_customer_id\", \"sc_cumulatedprice_swissrappen\") VALUES ('"+getscID()+"', '"+getUserID()+"', '"+0+"')";
                 System.out.println(query);
                 Statement pst = connection.createStatement();
                 pst.execute(query);
-                System.out.println("ID: "+newSCID);
+                System.out.println("ID: "+getscID());
             }
 
         }catch (SQLException e) {
@@ -84,11 +87,62 @@ public class PostgressMasterClass {
 
     }
 
-    private static void StringSplitter(String OrderChaos){
-        String[] getAllOrders = new String[15];
-        String firstPart = OrderChaos.split(";")[0];
-        System.out.println(firstPart.toString());
+    private static void orderCreation(String oC){
 
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/dd786fen583a5s?ssl=true\n", "mtqjbarwkwzsld", "2b51f440930a3711f9cabf9884d82feda3b70e9b35c9e9fb2d5f6f989fef445a"))
+        {
+            System.out.println("Java JDBC PostgreSQL Example");
+            System.out.println("Connected to PostgreSQL database!");
+
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select o_id   from orders ORDER BY o_id desc");
+
+            int currentOrderID;
+
+            if (rs.next()){
+                currentOrderID = rs.getInt("o_id")+1;
+            }
+            else{
+                currentOrderID = 0;
+            }
+
+            Map<String, String> cleanList = StringSplitter(oC);
+
+            for(Map.Entry<String, String> entry: cleanList.entrySet()) {
+                String query = "INSERT INTO orders (\"o_id\", \"o_amountofitems\", \"fk_item_id\", \"fk_customer_id\",\"fk_shoppingcarts_id\") VALUES ('"+currentOrderID+"','"+entry.getValue()+"','"+entry.getKey()+"','"+getUserID()+"', '"+getscID()+"')";
+                System.out.println(query);
+                Statement pst = connection.createStatement();
+                pst.execute(query);
+
+                currentOrderID++;
+            }
+
+
+
+        }catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+    private static Map<String, String> StringSplitter(String orderChaos) {
+
+        Map<String, String> ordersList = new HashMap<>();
+
+        String[] parts = orderChaos.split(";");
+
+        for (int i = 0; i < parts.length; i++) {
+
+            String[] order = parts[i].split("\\.");
+
+            ordersList.put(order[0], order[1]);
+        }
+
+        return ordersList;
     }
 
     public static int getUserID(){
@@ -97,5 +151,13 @@ public class PostgressMasterClass {
 
     public static void setUserID(int newUserID){
         userID = newUserID;
+    }
+
+    public static int getscID(){
+        return scID;
+    }
+
+    public static void setscID(int newscID){
+        scID = newscID;
     }
 }
